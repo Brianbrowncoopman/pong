cf90022bfcb2191882d6427dcf0cbe32784edc36
Brian Brown Coopman <brianbrowncoopman@gmail.com>
2023-01-25 19:09:51 -0300

    25-1

--- pong.py
+++ pong.py
@@ -1,32 +1,36 @@
 import turtle
 
-#ventana
+# ventana
 wn = turtle.Screen()
 wn.title("Pong by BBC")
 wn.bgcolor("black")
-wn.setup(width = 800, height = 600)
+wn.setup(width=800, height=600)
 wn.tracer(0)
 
+# Marcador
+marcadorA = 0
+marcadorB = 0
 
-#jugadar A
+
+# jugadar A
 jugadorA = turtle.Turtle()
 jugadorA.speed(0)
 jugadorA.shape("square")
 jugadorA.color("white")
 jugadorA.penup()
 jugadorA.goto(-350, 0)
-jugadorA.shapesize(stretch_wid = 5, stretch_len = 1)
+jugadorA.shapesize(stretch_wid=5, stretch_len=1)
 
-#jugadar B
+# jugadar B
 jugadorB = turtle.Turtle()
 jugadorB.speed(0)
 jugadorB.shape("square")
 jugadorB.color("white")
 jugadorB.penup()
 jugadorB.goto(350, 0)
-jugadorB.shapesize(stretch_wid = 5, stretch_len = 1)
+jugadorB.shapesize(stretch_wid=5, stretch_len=1)
 
-#pelota
+# pelota
 pelota = turtle.Turtle()
 pelota.speed(0)
 pelota.shape("circle")
@@ -36,78 +40,102 @@ pelota.goto(0, 0)
 pelota.dx = 3
 pelota.dy = 3
 
-#linea division
+# linea division
 division = turtle.Turtle()
 division.color("white")
 division.goto(0, 400)
 division.goto(0, -400)
 
+# pen
+pen = turtle.Turtle()
+pen.speed(0)
+pen.color("white")
+pen.penup()
+pen.hideturtle()
+pen.goto(0, 260)
+pen.write(
+    "jugador A: 0     jugador B: 0", align="center", font=("Courier", 24, "normal")
+)
 
-#funciones
-    #jugadorA
+
+# funciones
+# jugadorA
 def jugadorA_up():
     y = jugadorA.ycor()
     y += 20
     jugadorA.sety(y)
 
+
 def jugadorA_down():
     y = jugadorA.ycor()
     y -= 20
     jugadorA.sety(y)
-    
-    #jugadorB
+
+    # jugadorB
+
+
 def jugadorB_up():
     y = jugadorB.ycor()
     y += 20
     jugadorB.sety(y)
+
 
 def jugadorB_down():
     y = jugadorB.ycor()
     y -= 20
     jugadorB.sety(y)
 
-#teclado
+
+# teclado
 wn.listen()
-wn.onkeypress(jugadorA_up, "w")     #boton arriba jugadorA
-wn.onkeypress(jugadorA_down, "z")   #boton abajo jugadorA
-wn.onkeypress(jugadorB_up, "u")     #boton arriba jugadorB
-wn.onkeypress(jugadorB_down, "n")   #boton abajo jugadorB
+wn.onkeypress(jugadorA_up, "w")  # boton arriba jugadorA
+wn.onkeypress(jugadorA_down, "z")  # boton abajo jugadorA
+wn.onkeypress(jugadorB_up, "u")  # boton arriba jugadorB
+wn.onkeypress(jugadorB_down, "n")  # boton abajo jugadorB
 
 
-#motor del juego
+# motor del juego
 while True:
     wn.update()
 
     pelota.setx(pelota.xcor() + pelota.dx)
     pelota.sety(pelota.ycor() + pelota.dy)
 
-    #bordes
+    # bordes
     if pelota.ycor() > 270:
         pelota.dy *= -1
     if pelota.xcor() < -270:
         pelota.dy *= -1
 
-    #bordes deerecha/izquierda
+    # bordes deerecha/izquierda
     if pelota.xcor() > 380:
         pelota.goto(0, 0)
         pelota.dx *= -1
-        
+        marcadorA += 1
+        pen.clear()
+        pen.write(
+            "jugador A: {}     jugador B: {}".format(marcadorA, marcadorB),
+            align="center",
+            font=("Courier", 24, "normal"),
+        )
+
     if pelota.xcor() < -390:
         pelota.goto(0, 0)
         pelota.dx *= -1
+        marcadorB += 1
+        pen.clear()
+        pen.write(
+            "jugador A: {}     jugador B: {}".format(marcadorA, marcadorB),
+            align="center",
+            font=("Courier", 24, "normal"),
+        )
 
-    if ((pelota.xcor() > 340 and pelota.xcor() < 350)
-            and (pelota.ycor() < jugadorB.ycor() + 50
-            and pelota.ycor() > jugadorB.ycor() - 50)):
+    if (pelota.xcor() > 340 and pelota.xcor() < 350) and (
+        pelota.ycor() < jugadorB.ycor() + 50 and pelota.ycor() > jugadorB.ycor() - 50
+    ):
         pelota.dx *= -1
 
-    if ((pelota.xcor() < -340 and pelota.xcor() > -350)
-            and (pelota.ycor() < jugadorA.ycor() + 50
-            and pelota.ycor() > jugadorA.ycor() - 50)):
+    if (pelota.xcor() < -340 and pelota.xcor() > -350) and (
+        pelota.ycor() < jugadorA.ycor() + 50 and pelota.ycor() > jugadorA.ycor() - 50
+    ):
         pelota.dx *= -1
-
-
-
-
-        
-  
